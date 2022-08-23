813906f6753b62eb6e37636d0c82656c36a04b61
Jesiel021 <jesielcarlos17@gmail.com>
2022-08-23 19:17:26 -0300

    Removendo a SECRET_KEY

--- provtelecom/settings.py
+++ provtelecom/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q+94r#6fxqenty9m0_+^0r0)f7xglamt%=y9&qa942sey(g3cv'
+SECRET_KEY = decouple.config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
